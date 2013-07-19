f38db245bca5fdca3e139630fb978a41f411fd84
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-19 22:06:35 +0200

    adding home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tsadiabase::Application.routes.draw do
   resources :patients
 
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
