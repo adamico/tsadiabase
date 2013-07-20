bb5f74a02053a55d3c32728ba7a8d04d39e1319e
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-20 15:41:45 +0200

    tweaks to form, adding multiple buttons

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,64 +1,63 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :json
 
   # GET /patients
   # GET /patients.json
   def index
     @patients = Patient.all
+    respond_with @patients
   end
 
   # GET /patients/1
   # GET /patients/1.json
   def show
+    respond_with @patient
   end
 
   # GET /patients/new
   def new
     @patient = Patient.new
+    respond_with @patient
   end
 
   # GET /patients/1/edit
   def edit
+    respond_with @patient
   end
 
   # POST /patients
   # POST /patients.json
   def create
-    @patient = Patient.new(patient_params)
-
-    respond_to do |format|
-      if @patient.save
-        format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @patient }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    @patient = Patient.create(patient_params)
+    flash[:notice] = "Fiche créée avec succès" if @patient.save
+    if params[:_close]
+      location = patients_path
+    else
+      location = new_patient_path
     end
+    respond_with @patient, location: location
   end
 
   # PATCH/PUT /patients/1
   # PATCH/PUT /patients/1.json
   def update
-    respond_to do |format|
-      if @patient.update(patient_params)
-        format.html { redirect_to @patient, notice: 'Patient was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    if params[:_close]
+      location = patients_path
+    else
+      location = new_patient_path
     end
+    @patient.update(patient_params)
+    flash[:notice] = "Fiche mise à jour avec succès" if @patient.save
+    respond_with @patient, location: location
   end
 
   # DELETE /patients/1
   # DELETE /patients/1.json
   def destroy
     @patient.destroy
-    respond_to do |format|
-      format.html { redirect_to patients_url }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Fiche détruite avec succès"
+    respond_with @patient
   end
 
   private
