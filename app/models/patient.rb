be516ac8dfb23b222bcaf49427e6f85ae78628d5
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-20 20:00:02 +0200

    resultats ordered by libelle and cote

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
-  has_many :resultats, -> {order(:libelle)}, dependent: :destroy
+  has_many :resultats, -> {order("libelle ASC, cote DESC")}, dependent: :destroy
 
   accepts_nested_attributes_for :resultats, allow_destroy: true, reject_if: proc { |obj| obj.blank? }
 
