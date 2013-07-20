7a732cc4ca1c35d4dedf859027ea8b704f46680a
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-20 19:58:13 +0200

    order resultats by libelle

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
-  has_many :resultats, dependent: :destroy
+  has_many :resultats, -> {order(:libelle)}, dependent: :destroy
 
   accepts_nested_attributes_for :resultats, allow_destroy: true, reject_if: proc { |obj| obj.blank? }
 
